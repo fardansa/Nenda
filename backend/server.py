b8a66aeacc12691a5da2bf4bce7aae72637dfac5
Trystan Prastanov Gabriel Situmorang <titan2011official@gmail.com>
2026-06-11 13:55:37 +0700

    setup server

---
+++ backend/server.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("server:app", host="0.0.0.0", port=6969, reload=True)
